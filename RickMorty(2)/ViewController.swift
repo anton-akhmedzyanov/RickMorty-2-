9fe7ff4ed436d2662f5165b41e2da56b9a49844c
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2023-10-17 20:43:25 +0300

    Initial Commit

---
+++ RickMorty(2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMorty(2)
+//
+//  Created by Anton Akhmedzyanov on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
